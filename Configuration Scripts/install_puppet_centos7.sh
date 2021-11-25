69d3d7f83b153d4653e0ab409daec49fcaff73f2
Leeor Vardi <leeor.v@qualisystems.com>
2021-11-25 10:31:31 +0200

    added -y to yum call

--- Configuration Scripts/install_puppet_centos7.sh
+++ Configuration Scripts/install_puppet_centos7.sh
@@ -1,3 +1,3 @@
 sudo rpm -Uvh https://yum.puppet.com/puppet7-release-el-7.noarch.rpm
-sudo yum install puppet-agent
+sudo yum install puppet-agent -y
 sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
