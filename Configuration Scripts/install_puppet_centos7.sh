05e30cc42fe1c0286da9d1e3803b3c0e1ef0dc18
Leeor Vardi <leeor.v@qualisystems.com>
2021-11-26 21:17:40 +0200

    puppet install - lowered run interval from 30m to 1m

--- Configuration Scripts/install_puppet_centos7.sh
+++ Configuration Scripts/install_puppet_centos7.sh
@@ -1,3 +1,5 @@
 sudo rpm -Uvh https://yum.puppet.com/puppet7-release-el-7.noarch.rpm
 sudo yum install puppet-agent -y
+echo "[agent]">>/etc/puppetlabs/puppet/puppet.conf
+echo "  runinterval=1m">>/etc/puppetlabs/puppet/puppet.conf
 sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
